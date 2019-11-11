936f928e249c28bd8a00a83b9618068b476ec559
Timothy Mutegi <mutegi.timothy538@gmail.com>
2019-11-11 10:54:23 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { CardComponent } from './card/card.component';
+import { BranchComponent } from './branch/branch.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'branch', component: BranchComponent },
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SidebarComponent,
+    DashboardComponent,
+    AdminComponent,
+    CardComponent,
+    BranchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
